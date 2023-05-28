da46da7014d5d3fbe997861a3f5e390c2bb2c698
Nick Song <nick.song@klarna.com>
2023-05-28 11:48:13 +0200

    better looking popup and background js correction

--- background.js
+++ background.js
@@ -17,17 +17,19 @@ chrome.storage.onChanged.addListener(function (changes, namespace) {
       // Inform all tabs that options have been updated.
       chrome.tabs.query({}, function (tabs) {
         for (var i = 0; i < tabs.length; i++) {
-          chrome.storage.sync.get(["color", "behavior"], function (result) {
-            chrome.tabs.sendMessage(tabs[i].id, {
-              action: "updateOptions",
-              options: {
-                color: changes.color ? changes.color.newValue : result.color,
-                behavior: changes.behavior
-                  ? changes.behavior.newValue
-                  : result.behavior,
-              },
+          if (tabs[i].id) {
+            chrome.storage.sync.get(["color", "behavior"], function (result) {
+              chrome.tabs.sendMessage(tabs[i].id, {
+                action: "updateOptions",
+                options: {
+                  color: changes.color ? changes.color.newValue : result.color,
+                  behavior: changes.behavior
+                    ? changes.behavior.newValue
+                    : result.behavior,
+                },
+              });
             });
-          });
+          }
         }
       });
     }
